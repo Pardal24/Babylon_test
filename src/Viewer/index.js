5a2d08089a007ecf4cd80ad82b90ccf5a9c84376
Pardal24 <franciscopribeiro24@gmail.com>
2023-10-14 20:40:15 +0100

    cube with planes

--- src/Viewer/index.js
+++ src/Viewer/index.js
@@ -1,27 +1,51 @@
 import React, { Component } from 'react';
 import * as BABYLON from 'babylonjs';
+// import {Texture} from "@babylonjs/core/Materials/Textures"
 import BabylonScene from '../BabylonScene/'; // import the component above linking to file we just created.
 
 export default class Viewer extends Component<{}, {}> {
     onSceneMount = (e: SceneEventArgs) => {
         const { canvas, scene, engine } = e;
 
-        // This creates and positions a free camera (non-mesh)
-        const camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
-        // This targets the camera to scene origin
-        camera.setTarget(BABYLON.Vector3.Zero());
-        // This attaches the camera to the canvas
+        var camera = new BABYLON.ArcRotateCamera("Camera", -Math.PI / 2,  Math.PI / 2, 5, BABYLON.Vector3.Zero(), scene);
         camera.attachControl(canvas, true);
-        // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
-        const light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
-        // Default intensity is 1. Let's dim the light a small amount
-        light.intensity = 0.7;
-        // Our built-in 'sphere' shape. Params: name, subdivs, size, scene
-        const sphere = BABYLON.Mesh.CreateSphere("sphere1", 16, 2, scene);
-        // Move the sphere upward 1/2 its height
-        sphere.position.y = 1;
-        // Our built-in 'ground' shape. Params: name, width, depth, subdivs, scene
-        const ground = BABYLON.Mesh.CreateGround("ground1", 6, 6, 2, scene);
+        
+        var light = new BABYLON.HemisphericLight("hemiLight", new BABYLON.Vector3(-1, 1, 0), scene);
+        light.diffuse = new BABYLON.Color3(0.98, 0.98, 0.98);
+        
+        // // Skybox
+        // var skybox = BABYLON.MeshBuilder.CreateBox("skyBox", {size:1000.0}, scene);
+        // var skyboxMaterial = new BABYLON.StandardMaterial("skyBox", scene);
+        // skyboxMaterial.backFaceCulling = false;
+        // skyboxMaterial.reflectionTexture = new BABYLON.CubeTexture("textures/skybox", scene);
+        // skyboxMaterial.reflectionTexture.coordinatesMode = BABYLON.Texture.SKYBOX_MODE;
+        // skyboxMaterial.diffuseColor = new BABYLON.Color3(0, 0, 0);
+        // skyboxMaterial.specularColor = new BABYLON.Color3(0, 0, 0);
+        // skybox.material = skyboxMaterial;	
+    
+        
+        
+        var mat = new BABYLON.StandardMaterial("texture", scene);
+        mat.diffuseTexture = new BABYLON.Texture("/images/rock1.jpg",scene);
+        
+        var columns = 6;
+        var rows = 1;
+    
+        const faceUV = new Array(6);
+    
+        for (let i = 0; i < 6; i++) {
+            faceUV[i] = new BABYLON.Vector4(i / columns, 0, (i + 1) / columns, 1 / rows);
+        }
+        
+        const options = {
+            faceUV: faceUV,
+            wrap: true
+        };
+        
+        
+        var box = BABYLON.MeshBuilder.CreateBox("box", options);
+        box.material = mat;
+    
 
         engine.runRenderLoop(() => {
             if (scene) {
